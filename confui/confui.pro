2f5d58c7eae101c3803a63ca11315aec5cd6e1f0
lonkaars <loek@pipeframe.xyz>
2022-12-01 14:53:37 +0100

    folder restructure of confui

---
+++ confui/confui.pro
@@ -0,0 +1,11 @@
+QT += core gui widgets
+
+SOURCES += \
+    main.cpp \
+    mainwindow.cpp
+
+HEADERS += \
+    mainwindow.h
+
+CONFIG += c++17
+
--- confui/mainwindow.h
+++ confui/mainwindow.h
@@ -1,5 +1,4 @@
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
+#pragma once
 
 #include <QMainWindow>
 #include <QtSerialPort/QSerialPort>
@@ -10,18 +9,10 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
+class MainWindow : public QMainWindow {
+	Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
-
-private slots:
-    void on_pushButton_clicked();
-
-private:
-    Ui::MainWindow *ui;
+	MainWindow(QWidget *parent = nullptr);
+	~MainWindow();
 };
-#endif // MAINWINDOW_H
